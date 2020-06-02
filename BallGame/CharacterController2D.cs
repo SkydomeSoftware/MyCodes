18dfa9c17e01828e2bba98f004eef612adcf3e60
SkydomeSoftware <47296776+SkydomeSoftware@users.noreply.github.com>
2020-06-02 14:21:40 +0200

    Add files via upload

---
+++ BallGame/CharacterController2D.cs
@@ -0,0 +1,382 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using UnityEditor;
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController2D : MonoBehaviour
+{
+    public float jumpHeight, pushForce, rotationForce, moveSpeed;
+    public bool upCollider, downCollider, leftCollider, rightCollider, rightUpCollider, leftUpCollider,downRightCollider,downLeftCollider, platformCollider, enemyCollider;
+    public float UpColliderDistance, DownColliderDIstance, LeftColliderDistance, RightColliderDistance;
+    public bool isAIGoing;
+    public bool isFalling;
+    public bool AIGoLeft, AIGoRight;
+    public Toggle TupCollider, TdownCollider, TleftCollider, TrightCollider, TrightUpCollider, TleftUpCollider, TdownRightCollider, TdownLeftCollider, TenemyCollider;
+  
+
+    public bool doublejump;
+    public int Coins;
+    public Text CoinsText;
+
+    public Camera PlayerCamera;
+    public TextMesh UpText;
+    public Vector3 cameraOffset, UpTextOffset;
+    public float FollowSpeed;
+    public float angularDrag;
+    public LayerMask GroundLayer, EnemyLayer, PlatformLayer, CoinLayer, DirectionLayer, InfoLayer, FinishLayer;
+
+
+
+    public void UIToggles()
+    {
+        TupCollider.isOn = upCollider;
+        TdownCollider.isOn = downCollider;
+        TleftCollider.isOn = leftCollider;
+        TrightCollider.isOn = rightCollider;
+        TrightUpCollider.isOn = rightUpCollider;
+        TleftUpCollider.isOn = leftUpCollider;
+        TdownRightCollider.isOn = downRightCollider;
+        TdownLeftCollider.isOn = downLeftCollider;
+        TenemyCollider.isOn = enemyCollider;
+    }
+
+
+    private void Start()
+    {
+        GroundLayer = LayerMask.GetMask("Ground");
+        EnemyLayer = LayerMask.GetMask("Enemy");
+        PlatformLayer = LayerMask.GetMask("Platform");
+        CoinLayer = LayerMask.GetMask("Coin");
+        DirectionLayer = LayerMask.GetMask("Direction");
+        InfoLayer = LayerMask.GetMask("Info");
+        FinishLayer = LayerMask.GetMask("Finish");
+    }
+
+   
+
+   
+    public void Move(Vector3 direction, float rotationspeed, float force)
+    {
+        transform.position += direction * moveSpeed * Time.deltaTime;
+        transform.GetComponent<Rigidbody2D>().AddForce(direction * force);
+        transform.GetComponent<Rigidbody2D>().AddTorque(rotationspeed, ForceMode2D.Impulse);
+    }
+
+
+
+    public void jump()
+    {
+        if(downCollider == true || doublejump == false)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(this.GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            doublejump = true;
+        }
+        if(downCollider== false || doublejump == true)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(this.GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            doublejump = false;
+        }
+    }
+    public bool CheckCollider(Vector2 direction, float distance)
+    {
+        Debug.DrawRay(transform.position, direction* distance, Color.green);
+        if (Physics2D.Raycast(transform.position, direction, distance, GroundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public bool IsFalling()
+    {
+        if (this.GetComponent<Rigidbody2D>().velocity.y < -0.05)
+        {
+            cameraOffset.y = 0;
+            return true;
+
+        }
+        else
+        {
+            cameraOffset.y = 3;
+            return false;
+        }
+    }
+    public bool IsEnemyNearbly()
+    {
+        if (Physics2D.Raycast(transform.position, new Vector2(0.7f, 0), 3, EnemyLayer))
+        {
+
+            return true;
+        }
+        if (Physics2D.Raycast(transform.position, new Vector2(-0.7f, 0), -3, EnemyLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public bool IsInfoNearbly()
+    {
+        if (Physics2D.Raycast(transform.position, new Vector2(0.7f, 0), 3, InfoLayer))
+        {
+
+            return true;
+        }
+        if (Physics2D.Raycast(transform.position, new Vector2(-0.7f, 0), -3, InfoLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool isDoubleWallInLeft()
+    {
+
+        if (Physics2D.Raycast(transform.position + new Vector3(0, 1), new Vector2(-0.8f, 1f), 1.5f, GroundLayer) == true)
+        {
+            leftUpCollider = true;
+
+            if (leftCollider && AIGoLeft)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            leftUpCollider = false;
+            return false;
+        }
+        return false;
+    }
+    public bool isDoubleWallInRight()
+    {
+
+        if (Physics2D.Raycast(transform.position + new Vector3(0, 1), new Vector2(0.8f, 1f), 1.5f, GroundLayer) == true)
+        {
+            rightUpCollider = true;
+
+            if (rightCollider && AIGoRight)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            rightUpCollider = false;
+            return false;
+        }
+        return false;
+    }
+    public bool isWallLeftDown()
+    {
+        if (CheckCollider(new Vector2(-1f, -1f), 1.5f))
+        {
+           
+            downLeftCollider = true;
+            return true;
+        }
+        else
+        {
+         
+            downLeftCollider = false;
+            return false;
+        }
+    }
+    public bool isWallRightDown()
+    {
+        if (CheckCollider(new Vector2(1f, -1f), 1.5f))
+        {
+            downRightCollider = true;
+            return true;
+        }
+        else
+        {
+            downRightCollider = false;
+            return false;
+        }
+    }
+
+    public IEnumerator DoubleJumpAI()
+    {
+        jump();
+        yield return new WaitForSeconds(0.5f);
+        jump();
+    }
+    public void Go()
+    {
+        if (!rightCollider && AIGoRight == true)
+        {
+            Move(Vector3.right, 0.5f * -1, 1f);
+        }
+        if (!leftCollider && AIGoLeft == true)
+        {
+            Move(Vector3.left, 0.5f * 1, 1f);
+        }
+        if (isDoubleWallInLeft() && downCollider == true)
+        {
+            StartCoroutine(DoubleJumpAI());
+        }
+        if (isDoubleWallInRight() && downCollider == true)
+        {
+            StartCoroutine(DoubleJumpAI());
+        }
+        else if (rightCollider==true && downCollider == true && AIGoRight)
+        {
+            jump();
+        }
+        else if (leftCollider == true && downCollider == true && AIGoLeft)
+        {
+            jump();
+        }
+
+        if (!isWallRightDown() && AIGoRight && downCollider)
+        {
+            Debug.DrawRay(transform.position, new Vector2(1f, -0.8f) * 1.5f, Color.red, 20f);
+            jump();
+        }
+
+        if (!isWallLeftDown() && AIGoLeft && downCollider)
+        {
+            Debug.DrawRay(transform.position, new Vector2(-1f, -0.8f) * 1.5f, Color.yellow, 20f);
+            jump();
+        }
+
+        if(IsEnemyNearbly()&& downCollider==true)
+        { 
+            jump();
+        }
+            RaycastHit2D DirectionHit = Physics2D.CircleCast(transform.position, 1f, transform.forward, 1f, DirectionLayer);
+            {
+                if (DirectionHit.transform != null)
+                {
+                    if (DirectionHit.transform.gameObject.name == "left")
+                    {
+                        AIGoLeft = true;
+                        AIGoRight = false;
+                    }
+                    else
+                    {
+                        AIGoLeft = false;
+                        AIGoRight = true;
+                    }
+                }
+            }
+    }
+    public void Update()
+        {
+        downCollider =  CheckCollider(Vector2.down, DownColliderDIstance);
+        leftCollider = CheckCollider(Vector2.left, LeftColliderDistance);
+        rightCollider = CheckCollider(Vector2.right, RightColliderDistance);
+        upCollider = CheckCollider(Vector2.up, UpColliderDistance);
+        isFalling = IsFalling();
+        enemyCollider = IsEnemyNearbly();
+        followPlayer();
+        CheckLayer();
+        UIToggles();
+        if (isAIGoing)
+        {
+            Go();
+        }
+     
+
+
+        if (Input.GetKey(KeyCode.D) && rightCollider == false)
+        {
+            Move(Vector3.right, rotationForce * -1, pushForce);
+        }
+        if (Input.GetKey(KeyCode.A) && leftCollider == false)
+        {
+            Move(Vector3.left, rotationForce, pushForce);
+        }
+        if (Input.GetKeyDown(KeyCode.W) && downCollider == true)
+        {
+            jump();
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && downCollider == false && doublejump == true)
+        {
+            jump();
+            doublejump = false;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            
+            isAIGoing = true;
+            this.transform.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+    public void CheckLayer()
+    {
+
+        RaycastHit2D PlatformHit = Physics2D.CircleCast(transform.position, 1f, transform.forward, 1f, PlatformLayer);
+        {
+            if(PlatformHit.transform != null)
+            {
+                transform.parent = PlatformHit.transform;
+                platformCollider = true;
+            }
+            else
+            {
+                transform.parent = null;
+                platformCollider = false;
+            }
+        }
+        RaycastHit2D CoinHit = Physics2D.CircleCast(transform.position, 1f, transform.forward, 1f, CoinLayer);
+        {
+            if (CoinHit.transform !=null)
+            {
+                Destroy(CoinHit.transform.gameObject);
+                Coins += 1;
+                CoinsText.text = Coins.ToString();
+            
+            }
+        }
+
+        RaycastHit2D InfoHit = Physics2D.CircleCast(transform.position, 1f, transform.forward, 1f, InfoLayer);
+        {
+            if (InfoHit.transform != null && InfoHit.transform.gameObject.GetComponent<Info>().entered == false)
+            {
+                InfoHit.transform.gameObject.GetComponent<Info>().textmesh.gameObject.SetActive(true);
+                InfoHit.transform.gameObject.GetComponent<Info>().textmesh.text = InfoHit.transform.gameObject.GetComponent<Info>().info;
+
+                this.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+                isAIGoing = false;
+                InfoHit.transform.gameObject.GetComponent<Info>().entered = true;
+            }
+        }
+
+        RaycastHit2D FinishHit = Physics2D.CircleCast(transform.position, 1f, transform.forward, 1f, FinishLayer);
+        {
+            if (FinishHit.transform != null)
+            {
+                FinishHit.transform.gameObject.GetComponent<FinishMap>().OpenNextLevel();
+             
+            }
+        }
+
+        RaycastHit2D EnemyHit = Physics2D.CircleCast(transform.position, 0.7f, transform.forward, 0.7f, EnemyLayer);
+        {
+            if (EnemyHit.transform != null)
+            {
+                enemyCollider = true;
+                Application.LoadLevel(Application.loadedLevel);
+            } 
+        }
+    }
+    public void followPlayer()
+    {
+        if(AIGoRight)
+        PlayerCamera.transform.position = Vector3.Slerp(PlayerCamera.transform.position, transform.position + cameraOffset, FollowSpeed * Time.deltaTime);
+        if(AIGoLeft)
+        PlayerCamera.transform.position = Vector3.Slerp(PlayerCamera.transform.position, transform.position + new Vector3(cameraOffset.x*(-3), cameraOffset.y, cameraOffset.z) , FollowSpeed * Time.deltaTime);
+        UpText.transform.position = Vector3.Slerp(UpText.transform.position, transform.position + UpTextOffset, 15 * Time.deltaTime);
+    }
+
+}
---
+++ BallGame/ObjectBuilderEditor.cs
@@ -0,0 +1,291 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+
+
+
+
+
+
+
+
+[CustomEditor(typeof(CharacterController2D))]
+
+public class ObjectBuilderEditor0 : Editor
+{
+    bool ShowMovingProporties = true;
+    bool ShowCameraProporties = true;
+    bool ShowOtherProporties = true;
+    bool ShowDetectors = true;
+    bool ShowOthers = false;
+
+
+
+
+
+
+
+    public override void OnInspectorGUI()
+    {
+
+
+
+
+        GUIStyle HeaderStyle = new GUIStyle();
+        HeaderStyle.fontSize = 15;
+        HeaderStyle.fontStyle = FontStyle.Bold;
+
+        GUIStyle HeaderSmallStyle = new GUIStyle();
+        HeaderSmallStyle.fontSize = 11;
+        HeaderSmallStyle.fontStyle = FontStyle.Bold;
+
+
+
+        CharacterController2D Character = (CharacterController2D)target;
+        GUILayout.Space(10);
+
+       
+
+        EditorGUILayout.LabelField("Właściwości postaci:", HeaderStyle, GUILayout.Height(20));
+       
+        Character.gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)EditorGUILayout.ObjectField("Tekstura postaci:", Character.gameObject.GetComponent<SpriteRenderer>().sprite, typeof(Sprite), true);
+
+      
+
+
+        ShowMovingProporties = EditorGUILayout.Foldout(ShowMovingProporties, "Ustawienia poruszania się:");
+        if (ShowMovingProporties)
+            if (Selection.activeTransform)
+            {
+                
+                Character.jumpHeight = EditorGUILayout.Slider("Wysokość skoku:", Character.jumpHeight, 0, 30);
+              
+                Character.moveSpeed = EditorGUILayout.Slider("prędkość poruszania:", Character.moveSpeed, 0, 5);
+                Character.pushForce = EditorGUILayout.Slider("Siła pchnięcia:", Character.pushForce, 0, 5);
+       
+                Character.rotationForce = EditorGUILayout.Slider("Prędkość obrotu:", Character.rotationForce, 0, 3);
+              
+                Character.gameObject.GetComponent<Rigidbody2D>().mass = EditorGUILayout.FloatField("Masa postaci:", Character.gameObject.GetComponent<Rigidbody2D>().mass);
+                Character.gameObject.GetComponent<Rigidbody2D>().angularDrag = EditorGUILayout.FloatField("Siła tarcia:", Character.gameObject.GetComponent<Rigidbody2D>().angularDrag);
+          
+                GUILayout.Space(20);
+            }
+     
+        
+
+        ShowCameraProporties = EditorGUILayout.Foldout(ShowCameraProporties, "Ustawienia kamery:");
+        if (ShowCameraProporties)
+            if (Selection.activeTransform)
+            {
+                Character.cameraOffset = EditorGUILayout.Vector3Field("Pozycja kamery:", Character.cameraOffset);
+                Character.FollowSpeed = EditorGUILayout.Slider("Prędkość podążania kamery:", Character.FollowSpeed, 0, 10);
+                Character.PlayerCamera = (Camera)EditorGUILayout.ObjectField("Kamera", Character.PlayerCamera, typeof(Camera), true);
+                GUILayout.Space(20);
+            }
+    
+
+
+        ShowOtherProporties = EditorGUILayout.Foldout(ShowOtherProporties, "Ustawienia tekstu nad graczem");
+        if (ShowOtherProporties)
+            if (Selection.activeTransform)
+            {
+                Character.UpText = (TextMesh)EditorGUILayout.ObjectField("Nazwa postaci obiekt:", Character.UpText, typeof(TextMesh), true);
+                Character.UpTextOffset = EditorGUILayout.Vector3Field("Pozycja tekstu:", Character.UpTextOffset);
+                Character.UpText.text = EditorGUILayout.TextField("Nazwa gracza:", Character.UpText.text);
+                Character.UpText.color = EditorGUILayout.ColorField("Kolor tekstu", Character.UpText.color);
+               
+               
+                GUILayout.Space(20);
+            }
+
+        ShowDetectors = EditorGUILayout.Foldout(ShowDetectors, "Debbuger:");
+        if (ShowDetectors)
+            if (Selection.activeTransform)
+            {
+                EditorGUILayout.LabelField("Czujniki:", HeaderSmallStyle, GUILayout.Width(100));
+
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("", GUILayout.Width(66));
+                EditorGUILayout.LabelField("L.górny", GUILayout.Width(50));
+                EditorGUILayout.Toggle(Character.leftUpCollider, GUILayout.Width(25));
+                EditorGUILayout.LabelField("", GUILayout.Width(20));
+                EditorGUILayout.Toggle(Character.rightUpCollider, GUILayout.Width(25));
+                EditorGUILayout.LabelField("P.górny", GUILayout.Width(50));
+
+                EditorGUILayout.EndHorizontal();
+
+
+
+
+
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("", GUILayout.Width(66));
+                EditorGUILayout.LabelField("", GUILayout.Width(66));
+                EditorGUILayout.LabelField("Górny", GUILayout.Width(40));
+                Character.UpColliderDistance = EditorGUILayout.FloatField(Character.UpColliderDistance, GUILayout.Width(25));
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("", GUILayout.Width(66));
+                EditorGUILayout.LabelField("", GUILayout.Width(76));
+                Character.upCollider = EditorGUILayout.Toggle(Character.upCollider, GUILayout.Width(90));
+                EditorGUILayout.EndHorizontal();
+
+
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("", GUILayout.Width(66));
+                Character.LeftColliderDistance = EditorGUILayout.FloatField(Character.LeftColliderDistance, GUILayout.Width(25));
+                EditorGUILayout.LabelField("Lewy", GUILayout.Width(38));
+                Character.leftCollider = EditorGUILayout.Toggle(Character.leftCollider, GUILayout.Width(16));
+                Character.rightCollider = EditorGUILayout.Toggle(Character.rightCollider, GUILayout.Width(16));
+                EditorGUILayout.LabelField("Prawy", GUILayout.Width(45));
+                Character.RightColliderDistance = EditorGUILayout.FloatField(Character.RightColliderDistance, GUILayout.Width(25));
+                EditorGUILayout.EndHorizontal();
+
+
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("", GUILayout.Width(66));
+                EditorGUILayout.LabelField("", GUILayout.Width(76));
+                Character.downCollider = EditorGUILayout.Toggle(Character.downCollider, GUILayout.Width(90));
+                EditorGUILayout.EndHorizontal();
+
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("", GUILayout.Width(66));
+                EditorGUILayout.LabelField("", GUILayout.Width(66));
+                EditorGUILayout.LabelField("Dolny", GUILayout.Width(40));
+                Character.DownColliderDIstance = EditorGUILayout.FloatField(Character.DownColliderDIstance, GUILayout.Width(25));
+                EditorGUILayout.EndHorizontal();
+
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("", GUILayout.Width(66));
+                EditorGUILayout.LabelField("L.dolny", GUILayout.Width(50));
+
+                EditorGUILayout.Toggle(Character.downLeftCollider, GUILayout.Width(25));
+                EditorGUILayout.LabelField("", GUILayout.Width(20));
+                EditorGUILayout.Toggle(Character.downRightCollider, GUILayout.Width(25));
+                EditorGUILayout.LabelField("P.dolny", GUILayout.Width(50));
+
+                EditorGUILayout.EndHorizontal();
+
+             
+
+                GUILayout.Space(25);
+
+                EditorGUILayout.BeginHorizontal();
+
+                EditorGUILayout.LabelField("Postać spada:", GUILayout.Width(85));
+                EditorGUILayout.Toggle(Character.isFalling, GUILayout.Width(16));
+
+
+                EditorGUILayout.LabelField("Platforma:", GUILayout.Width(85));
+                Character.platformCollider = EditorGUILayout.Toggle(Character.platformCollider, GUILayout.Width(16));
+                EditorGUILayout.LabelField("Przeciwnik:", GUILayout.Width(65));
+                Character.enemyCollider = EditorGUILayout.Toggle(Character.enemyCollider, GUILayout.Width(16));
+                EditorGUILayout.EndHorizontal();
+
+
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Idzie w lewo:", GUILayout.Width(85));
+                Character.AIGoLeft = EditorGUILayout.Toggle(Character.AIGoLeft, GUILayout.Width(16));
+
+                EditorGUILayout.LabelField("Idzie w prawo:", GUILayout.Width(85));
+                Character.AIGoRight = EditorGUILayout.Toggle(Character.AIGoRight, GUILayout.Width(16));
+                
+
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Idzie:", GUILayout.Width(85));
+                Character.isAIGoing = EditorGUILayout.Toggle(Character.isAIGoing, GUILayout.Width(16));
+                EditorGUILayout.EndHorizontal();
+
+                GUILayout.Space(10);
+
+                Character.TdownCollider = (Toggle)EditorGUILayout.ObjectField("TdownCollider", Character.TdownCollider, typeof(Toggle), true);
+                Character.TleftCollider = (Toggle)EditorGUILayout.ObjectField("TleftCollider", Character.TleftCollider, typeof(Toggle), true);
+                Character.TrightCollider = (Toggle)EditorGUILayout.ObjectField("TrightCollider", Character.TrightCollider, typeof(Toggle), true);
+                Character.TrightUpCollider = (Toggle)EditorGUILayout.ObjectField("TrightUpCollider", Character.TrightUpCollider, typeof(Toggle), true);
+                Character.TleftUpCollider = (Toggle)EditorGUILayout.ObjectField("TleftUpCollider", Character.TleftUpCollider, typeof(Toggle), true);
+                Character.TdownRightCollider = (Toggle)EditorGUILayout.ObjectField("TdownRightCollider", Character.TdownRightCollider, typeof(Toggle), true);
+                Character.TdownLeftCollider = (Toggle)EditorGUILayout.ObjectField("TdownLeftCollider", Character.TdownLeftCollider, typeof(Toggle), true);
+                Character.TenemyCollider = (Toggle)EditorGUILayout.ObjectField("TenemyCollider", Character.TenemyCollider, typeof(Toggle), true);
+               
+               
+            }
+
+
+        ShowOthers = EditorGUILayout.Foldout(ShowDetectors, "Inne:");
+        if (ShowDetectors)
+            if (Selection.activeTransform)
+            {
+                Character.CoinsText = (Text)EditorGUILayout.ObjectField("Obiekt tekst monety", Character.CoinsText, typeof(Text), true);
+            }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+[CustomEditor(typeof(RotateObject))]
+public class ObjectBuilderEditor : Editor
+{
+
+    
+    public override void OnInspectorGUI()
+    {
+
+        GUIStyle HeaderStyle = new GUIStyle();
+        HeaderStyle.fontSize = 15;
+        HeaderStyle.fontStyle = FontStyle.Bold;
+        
+        
+        RotateObject RotationObject = (RotateObject)target;
+        GUILayout.Space(10);
+        EditorGUILayout.LabelField("Właściwości obrotu:", HeaderStyle, GUILayout.Height(20));
+        RotationObject.RotationSpeed = EditorGUILayout.Slider("Prędkość obrotu:", RotationObject.RotationSpeed, 0, 50);
+        RotationObject.turnRight = EditorGUILayout.Toggle("Obrót w prawo", RotationObject.turnRight);
+        RotationObject.turnLeft = EditorGUILayout.Toggle("Obrót w lewo", RotationObject.turnLeft);
+       
+    }
+}
+[CustomEditor(typeof(ScaleObject))]
+public class ObjectBuilderEditor2 : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        GUIStyle HeaderStyle = new GUIStyle();
+        HeaderStyle.fontSize = 15;
+        HeaderStyle.fontStyle = FontStyle.Bold;
+
+        DrawDefaultInspector();
+        ScaleObject ScaleObject = (ScaleObject)target;
+        GUILayout.Space(10);
+        EditorGUILayout.LabelField("Właściwości skalowania:", HeaderStyle, GUILayout.Height(20));
+        ScaleObject.ScallingSpeed = EditorGUILayout.Slider("Prędkość skalowania:", ScaleObject.ScallingSpeed, 0, 0.15f);
+        EditorGUILayout.LabelField("Minimalna wielkość:", ScaleObject.min.ToString("0.00"));
+        EditorGUILayout.LabelField("Maksymalna wielkość:", ScaleObject.max.ToString("0.00"));
+        EditorGUILayout.MinMaxSlider(ref ScaleObject.min, ref ScaleObject.max, 0, 2);
+     
+        if (GUILayout.Button("Zeruj ustawienia"))
+        {
+            ScaleObject.min = 0.8f;
+            ScaleObject.max = 1.2f;
+            ScaleObject.ScallingSpeed = 0;
+        }
+    }
+}
